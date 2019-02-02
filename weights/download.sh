6dd149251e59ee647f9ef327b228e996e635fd60
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-02-02 20:26:04 +0800

    feat: add download.sh to download yolo version 3 weights
    yolo version 3 has many weight that has been trained with coco dan voc dataset, it is best practice to use pretrained models if the data that we want to train is similar with coco or voc

---
+++ weights/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yolov3='https://pjreddie.com/media/files/yolov3.weights'
+yolov3_tiny='https://pjreddie.com/media/files/yolov3-tiny.weights'
+yolov3_spp='https://pjreddie.com/media/files/yolov3-spp.weights'
+
+wget $yolov3
+wget $yolov3_tiny
+wget $yolov3_spp
